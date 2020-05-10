3f8472bb9496b8b69b6aafa0d336b7d38c032772
Johan Bouduin <johan@der-e-coach.de>
2020-05-10 11:48:59 +0200

    #718: put logo in header

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -14,7 +14,14 @@ export class HeaderComponent implements OnInit {
   @Input() sidenav!: MatSidenav;
   // </editor-fold>
 
+  // <editor-fold desc='Private properties'>
+  private _headerLogoStyle: object;
+  // </editor-fold>
+
   // <editor-fold desc='Public getter methods'>
+  public get headerLogoStyle(): object {
+    return this._headerLogoStyle;
+  }
   public get routeLabelHome(): string {
     return this.translateService.instant('Navigation.RouteLabel.Home');
   }
@@ -25,11 +32,19 @@ export class HeaderComponent implements OnInit {
   // </editor-fold>
 
   // <editor-fold desc='Constructor and C°'>
-  constructor(private translateService: TranslateService) { }
+  constructor(private translateService: TranslateService) {
+    this._headerLogoStyle = {
+      'background-image': `url('assets/logo_40x40.png)`,
+      'background-position': 'center center',
+      'background-size': '40px 40px',
+      'background-repeat': 'no-repeat'
+      // 'margin-left': '-5px'
+    };
+  }
   // </editor-fold>
 
   // <editor-fold desc='Angular interface methods'>
-  ngOnInit() {}
+  ngOnInit() { }
   // </editor-fold>
 
 
