d431cdd214db736061dbdb9af4189eca69688b77
Johan Bouduin <johan@der-e-coach.de>
2023-01-06 16:20:50 +0100

    Restored routing

--- src/app/shell/connection/connection.component.ts
+++ src/app/shell/connection/connection.component.ts
@@ -9,17 +9,22 @@ import { GameService } from '../../game/game.service';
   templateUrl: './connection.component.html',
   styleUrls: ['./connection.component.scss']
 })
-export class ConnectionComponent  {
+export class ConnectionComponent {
 
-  //#region  Public getter methods
-  public get connectionIcon():string {
+  //#region private properties ------------------------------------------------
+  private translateService: TranslateService;
+  private connectionService: ConnectionService;
+  private gameService: GameService;
+  //#endregion
+
+  //#region Public getter methods ---------------------------------------------
+  public get connectionIcon(): string {
 
     switch (this.connectionService.connectionStatus) {
       case ConnectionStatus.Disconnected:
       case ConnectionStatus.Connecting:
       case ConnectionStatus.Countdown: {
         return 'cloud_off';
-        break;
       }
       case ConnectionStatus.Connected: {
         return 'cloud';
@@ -49,21 +54,20 @@ export class ConnectionComponent  {
   public get showReconnecting(): boolean {
     return this.connectionService.connectionStatus === ConnectionStatus.Countdown;
   }
-
   //#endregion
 
-  //#region  Constructor & C°
+  //#region Constructor & C° --------------------------------------------------
   public constructor(
-    private translateService: TranslateService,
-    private connectionService: ConnectionService,
-    private gameService: GameService) { }
+    translateService: TranslateService,
+    connectionService: ConnectionService,
+    gameService: GameService) {
+    this.translateService = translateService;
+    this.connectionService = connectionService;
+    this.gameService = gameService;
+  }
   //#endregion
 
-  //#region  Angular interface metods
-  // public ngOnInit(): void { }
-  //#endregion
-
-  //#region  UI triggered methods
+  //#region UI triggered methods ----------------------------------------------
   public reconnect(): void {
     this.gameService.rejoin();
   }
