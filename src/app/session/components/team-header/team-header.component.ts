b4c6bbadd3c48dcc3a71c3ac94fe57ac36345805
Johan Bouduin <johan@der-e-coach.de>
2023-01-25 16:34:05 +0100

    Extracted team header

--- src/app/session/components/team-header/team-header.component.ts
+++ src/app/session/components/team-header/team-header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Team } from '@app/session/objects';
 
 @Component({
   selector: 'session-team-header',
   templateUrl: './team-header.component.html',
-  styleUrls: ['./team-header.component.sass']
+  styleUrls: ['./team-header.component.scss']
 })
 export class TeamHeaderComponent {
 
+  // TODO replace by team service
+  @Input() team?: Team;
+
+  //#region getters -----------------------------------------------------------
+  public get teamName(): string {
+    return this.team?.teamName || 'unknown';
+  }
+  //#endregion
 }
