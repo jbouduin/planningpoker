be95b55ee4352ee8864f0403fc4f980c5f5b0abc
Johan Bouduin <johan@der-e-coach.de>
2023-03-02 09:10:10 +0100

    extended integration test leave

--- projects/server/test/integration/services/handler-leave.test.ts
+++ projects/server/test/integration/services/handler-leave.test.ts
@@ -4,11 +4,11 @@ import SERVICETYPES from '../../../src/services/service.types';
 
 import { IHandlerService } from '../../../src/services/interfaces';
 
-import { EClientMessageType, EMemberStatusChange, EServerMessageType, ILeaveMessage, IMemberChangedMessage } from '../../../../shared-lib/src';
+import { EClientMessageType, EErrorCode, EMemberStatusChange, EServerMessageType, ILeaveMessage, IMemberChangedMessage } from '../../../../shared-lib/src';
 import { Util } from "./util";
 
-describe('developer leaving', () => {
-  test('Standard => OK', () => {
+describe('developer leaving => OK', () => {
+  test('Standard', () => {
     const container = Util.getContainer();
     const handlerService = container.get<IHandlerService>(SERVICETYPES.HandlerService);
     // create team
@@ -107,7 +107,7 @@ describe('developer leaving', () => {
 
     // Test: observer should have received join messages + 1 disconnected member change + 1 leave memberchange
     expect(observerSend).toBeCalledTimes(Util.expectedMessagesJoin + 2);
-     expect(Util.countFilteredMessages<IMemberChangedMessage>(
+    expect(Util.countFilteredMessages<IMemberChangedMessage>(
       observerSend.mock.calls,
       EServerMessageType.MemberChanged,
       (m: IMemberChangedMessage) => m.data.memberStatusChange == EMemberStatusChange.Disconnected && participant1Id === participant1Id)
@@ -167,5 +167,112 @@ describe('scrum master leaving', () => {
     // participant should have received join messages + 1 join memberchange + 1 session ended
     expect(participantSend).toBeCalledTimes(Util.expectedMessagesJoin + 2);
     expect(Util.countMessageType(participantSend.mock.calls, EServerMessageType.EndSession)).toBe(1);
+  });
+});
+
+describe('leaving => failure', () => {
+  test('participant not found', () => {
+    const container = Util.getContainer();
+    const handlerService = container.get<IHandlerService>(SERVICETYPES.HandlerService);
+    // create team
+    const scrumMasterSend = jest.fn((_message: string) => Util.noop());
+    const scrumMasterSocket = Util.getSocket(scrumMasterSend);
+    Util.createTeam(scrumMasterSocket, handlerService, Util.team1Name, Util.scrumMaster1Nick);
+    // do not connect the user
+    const send2 = jest.fn((_message: string) => Util.noop());
+    const socket2 = Util.getSocket(send2);
+    // send leave message
+    const message: ILeaveMessage = {
+      senderId: 'some participant id',
+      type: EClientMessageType.Leave,
+      data: 'some participant id'
+    };
+    handlerService.handleMessage(message, Util.team1Name, socket2);
+    // participant should only have received the error message
+    expect(send2).toBeCalledTimes(1);
+    expect(Util.errorMessageReceived(send2.mock.calls, EErrorCode.ParticipantNotFound)).toBe(true);
+    // scrum master should not have received any additional messages
+    expect(scrumMasterSend).toBeCalledTimes(Util.expectedMessagesCreate);
+  });
+
+  test('team not found', () => {
+    const container = Util.getContainer();
+    const handlerService = container.get<IHandlerService>(SERVICETYPES.HandlerService);
+    // create team
+    const scrumMasterSend = jest.fn((_message: string) => Util.noop());
+    const scrumMasterSocket = Util.getSocket(scrumMasterSend);
+    Util.createTeam(scrumMasterSocket, handlerService, Util.team1Name, Util.scrumMaster1Nick);
+    // participant joins team 1
+    const participantSend = jest.fn((_message: string) => Util.noop());
+    const participantSocket = Util.getSocket(participantSend);
+    const participantId = Util.joinTeam(participantSocket, handlerService, Util.team1Name, Util.participant1Nick);
+    // send leave message for team 2
+    const message: ILeaveMessage = {
+      senderId: participantId,
+      type: EClientMessageType.Leave,
+      data: participantId
+    };
+    handlerService.handleMessage(message, Util.team2Name, participantSocket);
+    // participant should only have received the error message
+    expect(participantSend).toBeCalledTimes(Util.expectedMessagesJoin + 1);
+    expect(Util.errorMessageReceived(participantSend.mock.calls, EErrorCode.TeamDoesNotExist)).toBe(true);
+    // scrum master should not have received any additional messages
+    expect(scrumMasterSend).toBeCalledTimes(Util.expectedMessagesCreate + 1);
+  });
+
+  test('participant not in team', () => {
+    const container = Util.getContainer();
+    const handlerService = container.get<IHandlerService>(SERVICETYPES.HandlerService);
+    // create team
+    const scrumMasterSend = jest.fn((_message: string) => Util.noop());
+    const scrumMasterSocket = Util.getSocket(scrumMasterSend);
+    Util.createTeam(scrumMasterSocket, handlerService, Util.team1Name, Util.scrumMaster1Nick);
+    // participant connects
+    const participantSend = jest.fn((_message: string) => Util.noop());
+    const participantSocket = Util.getSocket(participantSend);
+    const participantId = handlerService.handleConnect(participantSocket).participantId;
+    // send leave message
+    const message: ILeaveMessage = {
+      senderId: participantId,
+      type: EClientMessageType.Leave,
+      data: participantId
+    };
+    handlerService.handleMessage(message, Util.team1Name, participantSocket);
+    // participant should only have received the init and the error message
+    expect(participantSend).toBeCalledTimes(2);
+    expect(Util.errorMessageReceived(participantSend.mock.calls, EErrorCode.ParticipantNotInTeam)).toBe(true);
+    // scrum master should not have received any additional messages
+    expect(scrumMasterSend).toBeCalledTimes(Util.expectedMessagesCreate);
+  });
+
+  test('participant in another team', () => {
+    const container = Util.getContainer();
+    const handlerService = container.get<IHandlerService>(SERVICETYPES.HandlerService);
+    // create team 1
+    const scrumMaster1Send = jest.fn((_message: string) => Util.noop());
+    const scrumMaster1Socket = Util.getSocket(scrumMaster1Send);
+    Util.createTeam(scrumMaster1Socket, handlerService, Util.team1Name, Util.scrumMaster1Nick);
+    // create team 2
+    const scrumMaster2Send = jest.fn((_message: string) => Util.noop());
+    const scrumMaster2Socket = Util.getSocket(scrumMaster2Send);
+    Util.createTeam(scrumMaster2Socket, handlerService, Util.team2Name, Util.scrumMaster1Nick);
+    // participant joins team 1
+    const participantSend = jest.fn((_message: string) => Util.noop());
+    const participantSocket = Util.getSocket(participantSend);
+    const participantId = Util.joinTeam(participantSocket, handlerService, Util.team1Name, Util.participant1Nick);
+    // send leave message for another team
+    const message: ILeaveMessage = {
+      senderId: participantId,
+      type: EClientMessageType.Leave,
+      data: participantId
+    };
+    handlerService.handleMessage(message, Util.team2Name, participantSocket);
+    // participant should only have received the init and the error message
+    expect(participantSend).toBeCalledTimes(Util.expectedMessagesJoin + 1);
+    expect(Util.errorMessageReceived(participantSend.mock.calls, EErrorCode.ParticipantNotInTeam)).toBe(true);
+    // scrum master 1 should only have received create messages and join member change
+    expect(scrumMaster1Send).toBeCalledTimes(Util.expectedMessagesCreate + 1);
+    // scrum master 2 should only have received create messages
+    expect(scrumMaster2Send).toBeCalledTimes(Util.expectedMessagesCreate);
   });
 });
